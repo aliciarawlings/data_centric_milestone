8728b4a5313179d7a652a9dcb7136ffa1f954367
aliciarawlings <lilyrawlings123@gmail.com>
2020-06-15 22:47:50 +0000

    created exercise.html page. Route was also created to render template

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import pymongo
 import os
+from flask import Flask, render_template, redirect,request, url_for 
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 MONGODB_URI = os.getenv("MONGO_URI")
 DBS_NAME = "Before&After"
 COLLECTION_NAME = "exercises"
 
+
+## Starting connection with MONGODB
 def mongo_connect(url):
     try:
         conn = pymongo.MongoClient(url)
@@ -17,3 +23,13 @@ conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
+@app.route('/')
+@app.route('/get_exercises')
+def get_exercises():
+    return render_template('exercises.html', recipes=mongo.db.exercises.find())
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
