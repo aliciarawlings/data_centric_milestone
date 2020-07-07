504c6ecce8094ae8f52433140f1c31d8ce11cb2c
aliciarawlings <lilyrawlings123@gmail.com>
2020-07-07 15:58:06 +0000

    like button updating how many clicks within the html

--- static/script/script.js
+++ static/script/script.js
@@ -1,8 +1,9 @@
-var button =0 ;
-document.getElementById("like_btn"),
-count = 0;
-button.onclick=function likes(){
-    count+=1;
-    button.innerHTML ="like me" + count;
-}
+let input = document.querySelector('.likenumber');
+btnAdd.addEventListener("click", ()=> {
+    input.value= parseInt(input.value) + 1;
+
+});
+
+
+
 
